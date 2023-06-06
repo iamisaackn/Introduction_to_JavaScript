40dba84bf4c4a5e7255d6bbf4ecc9ecb70b2edc2
Isaac Kinyanjui Ngugi <117882836+iamisaackn@users.noreply.github.com>
2023-06-06 15:38:58 +0300

    create

---
+++ Commenting.js
@@ -0,0 +1,3 @@
+// My name is Isaac Kinyanjui Ngugi
+
+/*I am a dual major, studying BSC. Computer Science and CFA. I have always had a strong interes in finance and investment. I would like to collaborate Tech and Finance to create bi-products that can revolutionize the investment sector.*/
